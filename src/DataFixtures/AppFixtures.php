d7668b6d1253755e873dfef84838173a5c95caac
Clément Gaugain <gaugainc@yahoo.com>
2021-11-15 13:31:50 +0100

    Création de nos fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker = Factory::create("fr_FR");
+
+        // Création entre 15 et 30 tâches aléatoirement 
+        for ($t = 0; $t < mt_rand(12, 30); $t++) {
+            // Création d'un nouvel objet task 
+            $task = new Task;
+
+            // On nourrit l'objet Task
+            $task->setName($faker->sentence(6))
+                ->setDescription($faker->paragraph(3))
+                ->setCreatedAt(new \DateTime()) // Attention les dates
+                ->setDueAt($faker->dateTimeBetween("now", "6 months"));
+
+            // On fait persister les données
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
