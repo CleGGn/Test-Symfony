9bbb191bb8844e6c7a240080ca6e051a0da13d82
Clément Gaugain <gaugainc@yahoo.com>
2021-11-15 15:02:06 +0100

    Bootstrap

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -30,7 +30,7 @@ class TaskController extends AbstractController
         //dd($tasks);
 
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
 }
