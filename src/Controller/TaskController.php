4b4ead6bb1684ffb7ebe00f2f340fc8d663b7c2f
Clément Gaugain <gaugainc@yahoo.com>
2021-11-16 16:11:05 +0100

    Union des fonctions Create et Update

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -56,12 +56,14 @@ class TaskController extends AbstractController
 
     /**
      * @Route ("/task/create", name="task_create")
+     * @Route ("/task/update/{id}", name="task_update", requirements={"id"="\d+"})
      */
-    public function createTask(Request $request)
+    public function task(Task $task = null, Request $request)
     {
-        $task = new Task;
-
-        $task->setCreatedAt(new \DateTime());
+        if (!$task) {
+            $task = new Task;
+            $task->setCreatedAt(new \DateTime());
+        }
 
         $form = $this->createForm(TaskType::class, $task, []);
 
@@ -75,56 +77,6 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route ("/task/update/{id}", name="task_update", requirements={"id"="\d+"})
-     */
-    public function updateTask(int $id, Request $request): Response
-    {
-        $task = $this->repository->find($id);
-
-        $form = $this->createForm(TaskType::class, $task, []);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-
-            $this->manager->persist($task);
-            $this->manager->flush();
-
-            return $this->redirectToRoute(('task_update'));
-        }
-        return $this->render('task/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-
-
-    /**
-     * @Route ("/task/delete/{id}", name="delete", requirements={"id"="\d+"})
-     */
-    public function deleteTask(int $id, Request $request)
-    {
-        $task = $this->getDoctrine()->getRepository(Task::class)->find($id);
-
-        $form = $this->createForm(TaskType::class, $task, []);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($task);
-            $manager->flush();
-
-            return $this->redirectToRoute(('task_delete'));
-        }
-        return $this->render('task/delete.html.twig', [
             'form' => $form->createView()
         ]);
     }
